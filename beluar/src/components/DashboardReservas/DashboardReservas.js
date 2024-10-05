2cd38b15797d02c5868ba7981b56be53ab34e772
ferlongin <ferlongin@hotmail.com>
2024-10-05 12:34:00 -0300

    Finish dashboardReservas

--- beluar/src/components/DashboardReservas/DashboardReservas.js
+++ beluar/src/components/DashboardReservas/DashboardReservas.js
@@ -6,11 +6,54 @@ import Reserva from './Reserva';
 import ReservaCard from './ReservaCard';
 
 const reservaData = [
-    { number: '145', date: '15 Sep', name: 'Fernandez, Martin', price: 120, room: '#3B' },
-    { number: '467', date: '16 Sep', name: 'Rodriguez, Carla', price: 85, room: '#7J' },
-    { number: '689', date: '18 Sep', name: 'Gomez, Luis', price: 65, room: '#5C' },
-    { number: '999', date: '20 Sep', name: 'Suares, Natalia', price: 95, room: '#6A' },
-  ];
+    { 
+        number: '145', 
+        date: '15 Sep', 
+        name: 'Fernandez, Martin', 
+        price: 120, 
+        room: '#3B',
+        services: [
+            { name: 'Desayuno incluido', price: 20 },
+            { name: 'Acceso al gimnasio', price: 10 }
+        ],
+        abonar: 85,
+        checkIn: '18/09/2024',
+        checkOut: '21/09/2024'
+    },
+    { 
+        number: '467', 
+        date: '16 Sep', 
+        name: 'Rodriguez, Carla', 
+        price: 85, 
+        room: '#7J',
+        services: [
+            { name: 'Spa', price: 40 }
+        ],
+        abonar: 85
+    },
+    { 
+        number: '689', 
+        date: '18 Sep', 
+        name: 'Gomez, Luis', 
+        price: 65, 
+        room: '#5C',
+        services: [{ name: 'Spa', price: 40 }] ,
+        abonar: 85
+    },
+    { 
+        number: '999', 
+        date: '20 Sep', 
+        name: 'Suares, Natalia', 
+        price: 95, 
+        room: '#6A',
+        services: [
+            { name: 'Traslado al aeropuerto', price: 50 },
+            { name: 'Desayuno incluido', price: 20 }
+        ],
+        abonar: 85
+    }
+];
+
 
 function DashboardReservas() {
     const [selectedReserva, setSelectedReserva] = useState(reservaData[0]); 
--- beluar/src/components/DashboardReservas/ReservaCard.js
+++ beluar/src/components/DashboardReservas/ReservaCard.js
@@ -1,50 +1,60 @@
 import React from 'react';
+import './reserva.css';
 
 const ReservaCard = ({ item }) => {
   return (
-    <div className="item-detail">
-      <h3 className="item-number">Reserva # {item.number}</h3>
-      <div className='fields'>
+    <div className="reserva-detail">
+      <h3 className="reserva-number">Reserva #{item.number}</h3>
+      <div className="reserva-fields">
 
-        <div className="editable-fields">
-            <label>Nombre:</label>
-            <span className="editable-input">{item.name}</span>
+        <div className="reserva-field">
+          <label>Apellido y nombre</label>
+          <input type="text" value={item.name} className="reserva-input" readOnly />
         </div>
+
+        <div className="reserva-field">
+          <label>Habitación</label>
+          <input type="text" value={item.room} className="reserva-input" readOnly />
+        </div>
+
+        <div className="reserva-field">
+          <label>Check-in</label>
+          <input type="text" value={item.checkIn} className="reserva-input" readOnly />
+        </div>
+
+        <div className="reserva-field">
+          <label>Check-out</label>
+          <input type="text" value={item.checkOut} className="reserva-input" readOnly />
+        </div>
+
+        {/* Agregar la validación para services */}
+        {item.services && item.services.length > 0 && (
+          <div className="reserva-field">
+            <label>Servicios contratados</label>
+            <ul className="reserva-services">
+              {item.services.map((service, index) => (
+                <li key={index}>{service.name} <span className="service-price">USD {service.price}</span></li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <hr className="divider-line" /> 
         
-        <div className="editable-fields">
-            <label>Habitación:</label>
-            <span className="editable-input">{item.room}</span>
+        <div className="reserva-total">
+          <span>Total</span> <span className="reserva-total-price">USD {item.price}</span>
         </div>
 
-        <div className="editable-fields">
-            <label>Check-in:</label>
-            <span className="editable-input">{item.checkIn}</span>
+        <div className="reserva-remaining">
+          <span>Falta abonar</span> <span className="reserva-remaining-price">USD {item.abonar}</span>
         </div>
 
-        <div className="editable-fields">
-            <label>Check-out:</label>
-            <span className="editable-input">{item.checkOut}</span>
-        </div>
-
-        {/*<div className="editable-fields">
-            <label>Servicios Contratados:</label>
-            <span className="editable-input">{item.services.join(', ')}</span>
-        </div>*/}
-
-        <div className="editable-fields">
-            <label>Total:</label>
-            <span className="editable-input">${item.total}</span>
-        </div>
-
-        <div className="editable-fields">
-            <label>Falta Abonar:</label>
-            <span className="editable-input">${item.remainingBalance}</span>
-        </div>
-    </div>
-      <div className="buttons-reserva">
-        <button className="status-button">Abonar Total</button>
-        <button className="status-button">Agregar Servicio</button>
-        <button className="delete-button">Cancelar Reserva</button>
+      </div>
+      
+      <div className="reserva-buttons">
+        <button className="btn-reserva abonar">Abonar total</button>
+        <button className="btn-reserva agregar-servicio">Agregar servicio</button>
+        <button className="btn-reserva cancelar">Cancelar reserva</button>
       </div>
     </div>
   );
