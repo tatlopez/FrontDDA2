43d8f9c80dbf09d9ae6c5bd1788976fccb395b86
tatlopez <tatianalopez0727@gmail.com>
2024-10-15 20:23:28 -0300

    conectado borrar hotel

--- beluar/src/components/DashboardHoteles/DashboardHoteles.js
+++ beluar/src/components/DashboardHoteles/DashboardHoteles.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './dashboardHoteles.css';
 import Profile from '../Profile/Profile';
 import logo from '../../assets/logo.png';
@@ -8,35 +8,34 @@ import editIcon from '../../assets/edit-icon.png';
 import addIcon from '../../assets/signo-mas.png';
 import searchIcon from '../../assets/search-icon-white.svg';
 import EditarHotelModal from './EditarHotel';
-import ConfirmActionModal from '../PopUp/ConfirmActionModal'; 
+import ConfirmActionModal from '../PopUp/ConfirmActionModal';
 import cargarImagen from '../../assets/cargar-imagen.png';
+import delete_hotel from '../../services/hotels/delete_hotel';
 import SearchBar from '../SearchBar/SearchBar';
-import { useEffect } from 'react';
 import get_hotels from '../../services/hotels/get_hotels';
 import { API_URL } from '../../config';
 
-const hotelesData = [
-    { id: 1, nombre: 'Palacio del Sol', direccion: 'Av. Alvear 1661, Recoleta, CABA, Buenos Aires.', estrellas: 5 },
-    { id: 2, nombre: 'Estancia La Montaña', direccion: 'Ruta Nacional 40, km 65, San Martín de los Andes, Neuquén.', estrellas: 4 },
-    { id: 3, nombre: 'Hotel Imperial', direccion: 'Av. Maipú 505, Ushuaia, Tierra del Fuego.', estrellas: 4 },
-    { id: 4, nombre: 'Resort Las Águilas', direccion: 'Av. Bustillo km 25, San Carlos de Bariloche, Río Negro.', estrellas: 4 }
-];
-
-
 function DashboardHoteles() {
-    const [hoteles, setHoteles] = useState([]);
+    const [hoteles, setHoteles] = useState([]); // Inicializado como array vacío
     const [selectedHotel, setSelectedHotel] = useState(null);
     const [showModal, setShowModal] = useState(false);
     const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [showSearchBar, setShowSearchBar] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        get_hotels().then((res) => {
-            console.log('Hoteles cargados:', res);
-            setHoteles(res);
-        })
-    }, []);  
+        get_hotels()
+            .then((res) => {
+                setHoteles(res || []); 
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError('Hubo un error al cargar los hoteles. Inténtalo más tarde.');
+                setLoading(false);
+            });
+    }, []);
 
     const openModal = (hotel) => {
         setSelectedHotel(hotel);
@@ -44,7 +43,18 @@ function DashboardHoteles() {
     };
 
     const addNewHotel = () => {
-        const emptyHotel = { id: Date.now(), name: '', address: '', stars: 0, phone: '', email: '', description: '', latitude: '', longitude: '',imagen: cargarImagen };
+        const emptyHotel = { 
+            id: Date.now(), 
+            name: '', 
+            address: '', 
+            stars: 0, 
+            phone: '', 
+            email: '', 
+            description: '', 
+            latitude: '', 
+            longitude: '', 
+            imagen: cargarImagen 
+        };
         setSelectedHotel(emptyHotel);
         setShowModal(true);
     };
@@ -63,21 +73,28 @@ function DashboardHoteles() {
     };
 
     const handleSaveHotel = (updatedHotel) => {
-        
+        // Lógica para guardar el hotel actualizado
     };
 
     const handleDeleteHotel = (hotelToDelete) => {
-        setHoteles(hoteles.filter(hotel => hotel.id !== hotelToDelete.id));
-        closeDeleteModal(); // Aseguramos que el modal se cierre después de eliminar el hotel
+        delete_hotel(hotelToDelete.id)
+            .then(() => {
+                setHoteles(hoteles.filter(hotel => hotel.id !== hotelToDelete.id));
+                closeDeleteModal();
+            })
+            .catch((err) => {
+                setError('Hubo un problema al eliminar el hotel. Inténtalo de nuevo.');
+                closeDeleteModal();
+            });
     };
 
     const toggleSearchBar = () => {
         setShowSearchBar(!showSearchBar);
     };
 
-    const filteredHoteles = hoteles.filter(hotel =>
+    const filteredHoteles = Array.isArray(hoteles) ? hoteles.filter((hotel) =>
         hotel.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    ) : [];
 
     return (
         <div style={{ backgroundColor: '#FEFBFF' }}>
@@ -88,14 +105,14 @@ function DashboardHoteles() {
                 </div>
             </header>
             <main className="dashboard-container">
-                <div className='hoteles-title'>
+                <div className="hoteles-title">
                     <h1 style={{ textAlign: 'left' }}>Tus hoteles</h1>
-                    <div className='hoteles-buttons'>
-                        <button className='hoteles-circleButton' onClick={addNewHotel}>
-                            <img src={addIcon} alt='Agregar hotel' />
+                    <div className="hoteles-buttons">
+                        <button className="hoteles-circleButton" onClick={addNewHotel}>
+                            <img src={addIcon} alt="Agregar hotel" />
                         </button>
-                        <button className='hoteles-circleButton' onClick={toggleSearchBar}>
-                            <img src={searchIcon} alt='Buscar hotel' />
+                        <button className="hoteles-circleButton" onClick={toggleSearchBar}>
+                            <img src={searchIcon} alt="Buscar hotel" />
                         </button>
                     </div>
                 </div>
@@ -103,33 +120,43 @@ function DashboardHoteles() {
                     <SearchBar setSearchTerm={setSearchTerm} style={{ marginBottom: '20px' }} placeholder="Buscar hotel..." />
                 )}
                 <div className="hoteles-list">
-                    {filteredHoteles.map((hotel) => (
-                        <div key={hotel.id} className="hotel-item">
-                        <img src={hotel.images && hotel.images.length > 0 
-                                    ? `${API_URL}${hotel.images[0].image}` 
-                                    : hotelIcon // Usa la imagen predeterminada si no hay imágenes
-                        } 
-                alt={hotel.name} 
-                className="hotel-image" 
-            />
-                            <div className="hotel-info">
-                                <h2>{hotel.name}</h2>
-                                <p>{hotel.address + ', ' + hotel.city}</p>
-                                <div className="hotel-stars">
-                                    {'★'.repeat(hotel.stars)}
-                                    {'☆'.repeat(5 - hotel.stars)}
+                    {loading ? (
+                        <p>Cargando hoteles...</p> 
+                    ) : error ? (
+                        <p className="error-message">{error}</p> 
+                    ) : filteredHoteles.length === 0 ? (
+                        <p>No se encontraron hoteles.</p> 
+                    ) : (
+                        filteredHoteles.map((hotel) => (
+                            <div key={hotel.id} className="hotel-item">
+                                <img
+                                    src={
+                                        hotel.images && hotel.images.length > 0
+                                            ? `${API_URL}${hotel.images[0].image}`
+                                            : hotelIcon 
+                                    }
+                                    alt={hotel.name}
+                                    className="hotel-image"
+                                />
+                                <div className="hotel-info">
+                                    <h2>{hotel.name}</h2>
+                                    <p>{hotel.address + ', ' + hotel.city}</p>
+                                    <div className="hotel-stars">
+                                        {'★'.repeat(hotel.stars)}
+                                        {'☆'.repeat(5 - hotel.stars)}
+                                    </div>
+                                </div>
+                                <div className="hotel-actions">
+                                    <button className="edit-btn" onClick={() => openModal(hotel)}>
+                                        <img src={editIcon} alt="Edit" className="edit-image" />
+                                    </button>
+                                    <button className="delete-btn" onClick={() => openDeleteModal(hotel)}>
+                                        <img src={trashIcon} alt="Delete" className="trash-image" />
+                                    </button>
                                 </div>
                             </div>
-                            <div className="hotel-actions">
-                                <button className="edit-btn" onClick={() => openModal(hotel)}>
-                                    <img src={editIcon} alt="Edit" className="edit-image" />
-                                </button>
-                                <button className="delete-btn" onClick={() => openDeleteModal(hotel)}>
-                                    <img src={trashIcon} alt="Delete" className="trash-image" />
-                                </button>
-                            </div>
-                        </div>
-                    ))}
+                        ))
+                    )}
                 </div>
             </main>
             {showModal && (
--- beluar/src/services/hotels/delete_hotel.js
+++ beluar/src/services/hotels/delete_hotel.js
@@ -5,7 +5,7 @@ function delete_hotel(id, name, address, city, phone, email, description, stars,
 
     return (
 
-        fetch(`${API_URL}/delete-hotel/${id}`, {
+        fetch(`${API_URL}/delete-hotel/${id}/`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
