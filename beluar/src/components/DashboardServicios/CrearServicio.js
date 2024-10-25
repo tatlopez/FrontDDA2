781aeeb1ce35ce7b54fb8b4a97ce82d95aabcf01
Cami Gaitan <camigaitan2002@gmail.com>
2024-10-24 23:32:56 -0300

    fix modal crear servicio

--- beluar/src/components/DashboardServicios/CrearServicio.js
+++ beluar/src/components/DashboardServicios/CrearServicio.js
@@ -27,43 +27,51 @@ const AgregarServicioModal = ({ onClose, onSave }) => {
   return (
     <div className="modal-overlay">
       <div className="modal">
-        <button className="close-modal" onClick={onClose}>X</button>
-        <h2 className="modal-title">Agregar Servicio</h2>
+        <p className="modal-title">Agregar servicio</p>
         <div className="modal-content">
-          <div className="modal-field">
-            <label>Nombre</label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="modal-field">
-            <label>Detalles</label>
-            <input
-              type="text"
-              value={detail}
-              onChange={(e) => setDetail(e.target.value)}
-            />
-          </div>
-          <div className="modal-field">
-            <label>Disponibilidad</label>
-            <select value={is_available} onChange={(e) => setIsAvailable(e.target.value)} className="editable-select">
-              <option value="true">Habilitado</option>
-              <option value="false">Deshabilitado</option>
-            </select>
-          </div>
-          <div className="modal-field">
-            <label>Precio</label>
-            <input
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
+          <div className='modal-fields'>
+            <div className='fields-row'>
+              <div className="modal-field">
+                <label>Nombre</label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="modal-field">
+                <label>Detalles</label>
+                <input
+                  type="text"
+                  value={detail}
+                  onChange={(e) => setDetail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className='fields-row'>
+              <div className="modal-field">
+                <label>Disponibilidad</label>
+                <div className='select-wrapper'>
+                  <select value={is_available} onChange={(e) => setIsAvailable(e.target.value)} className="editable-select">
+                    <option value="true">Habilitado</option>
+                    <option value="false">Deshabilitado</option>
+                  </select>
+                </div>
+              </div>
+              <div className="modal-field">
+                <label>Precio</label>
+                <input
+                  type="number"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+            </div>
           </div>
         </div>
         <div className="modal-buttons">
-          <button className="btn-confirm" onClick={handleSave}>Agregar Servicio</button>
+          <button className="btn-confirm" onClick={handleSave}>Agregar Habitación</button>
+          <button className="btn-confirm" onClick={onClose}>Volver atrás</button>
         </div>
       </div>
     </div>
