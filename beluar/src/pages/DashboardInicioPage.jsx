ff035831830dc6586ad335e3c08cd28b72888d5a
tatlopez <tatianalopez0727@gmail.com>
2024-10-31 23:27:30 -0300

    arreglo

--- beluar/src/pages/DashboardInicioPage.jsx
+++ beluar/src/pages/DashboardInicioPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DashboardInicio from '../components/DashboardInicio/DashboardInicio.js'
+import DashboardInicio from '../components/DashboardInicio/dashboardInicio.js'
 
 function DashboardInicioPage() {
     return (
